306b9ca40dfd0b616b0bceed0c8c6d85c9e7856e
Tanner Smith <smithtannera@wifi_stu-10-38-6-125.uncc.edu>
2017-08-31 19:29:26 -0400

    first commit

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,10 @@
+var path = require("path");
+
+module.exports = function(app) {
+  app.get("/skyBio", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/skyBio.html"));
+  });
+  app.get("/facePlusPlus", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/facePlusPlus.html"));
+  });
+};
