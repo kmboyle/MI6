cfbb2650f678257579e0c35a03b9cc7257f1a437
Tanner Smith <smithtannera@wifi_stu-10-38-13-78.uncc.edu>
2017-09-07 19:41:46 -0400

    stuff

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 var express = require("express");
-var bodyParser = require("body-parser");
+//var bodyParser = require("body-parser");
 var request=require('request');
 
-var app = express();
+//var app = express();
 
-var PORT = process.env.PORT || 8080;
+//var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.text());
+//app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 //require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
+//require("./routes/htmlRoutes")(app);
+//app.listen(PORT, function() {
+  //console.log("App listening on PORT: " + PORT);
+//});
 
 
 
-var appp=express();
-appp.use('/',function(req,res){
+var app=express();
+app.use('/',function(req,res){
   var url = apiServerHost+req.url;
   req.pipe(request(url)).pipe(res);
 });
 
-appp.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
 
